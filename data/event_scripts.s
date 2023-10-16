01cd28acc7b0618d7c7d114776f76dd6cd16b516
kyuzeemctree <147213723+kyuzeemctree@users.noreply.github.com>
2023-10-16 08:06:09 -0500

    Added Dewford Grotto

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1061,3 +1061,5 @@ Common_EventScript_LegendaryFlewAway::
 	.include "data/text/save.inc"
 	.include "data/text/birch_speech.inc"
     .include "data/scripts/qol_field_move_scripts.inc" @ qol_field_moves 
+
+	.include "data/maps/DewfordGrotto/scripts.inc"
